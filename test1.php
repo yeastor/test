b3bee4e040fbff6c040159b97048448cbde85fe5
KupriyanovYM <kupriyanovym@gmail.com>
2016-09-20 12:33:49 +0300

    test 1

--- test1.php
+++ test1.php
@@ -4,6 +4,23 @@
  */
 $x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
+
+//$x = array_flip($x);
+$a = &$x;
+$c = count($x);
+for($i = 0;$i < $c;$i++ ){
+
+    end($a);
+    $k = $a[key($a)];
+
+    $a[$k] = array_splice($a,0,count($a)-1);
+    unset($a[0]);
+    $a =&$a[$k];
+
+}
+unset($a);
+print_r($x);
+
 /*
 print_r($x) - должен выводить это:
 Array
